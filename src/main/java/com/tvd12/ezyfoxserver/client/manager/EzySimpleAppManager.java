8aff5131609a51d7fb9e526a054bc50c880e70f8
Dung Ta Van <itprono3@gmail.com>
2019-11-07 21:24:40 +0700

    update app manager

--- src/main/java/com/tvd12/ezyfoxserver/client/manager/EzySimpleAppManager.java
+++ src/main/java/com/tvd12/ezyfoxserver/client/manager/EzySimpleAppManager.java
@@ -13,78 +13,79 @@ import com.tvd12.ezyfoxserver.client.entity.EzyApp;
 
 public class EzySimpleAppManager implements EzyAppManager {
 
-    protected final String zoneName;
-    protected final List<EzyApp> appList;
-    protected final Map<Integer, EzyApp> appsById;
-    protected final Map<String, EzyApp> appsByName;
+	protected final String zoneName;
+	protected final List<EzyApp> appList;
+	protected final Map<Integer, EzyApp> appsById;
+	protected final Map<String, EzyApp> appsByName;
 
-    public EzySimpleAppManager(String zoneName) {
-        this.zoneName = zoneName;
-        this.appList = new ArrayList<>();
-        this.appsById = new HashMap<>();
-        this.appsByName = new HashMap<>();
-    }
-
-    @Override
-    public void addApp(EzyApp app) {
-    	synchronized (this) {
-    		 this.appsById.put(app.getId(), app);
-    		 this.appsByName.put(app.getName(), app);
-		}
-    }
-    
-    @Override
-	public EzyApp removeApp(int appId) {
-    	synchronized (this) {
-	    	EzyApp app = this.appsById.remove(appId);
-	    	if(app != null) {
-	    		this.appList.remove(app);
-	    		this.appsByName.remove(app.getName());
-	    	}
-	    	return app;
-    	}
+	public EzySimpleAppManager(String zoneName) {
+		this.zoneName = zoneName;
+		this.appList = new ArrayList<>();
+		this.appsById = new HashMap<>();
+		this.appsByName = new HashMap<>();
 	}
 
-    @Override
-    public EzyApp getApp() {
-    	synchronized (this) {
-	        if(appList.isEmpty())
-	            throw new IllegalStateException("has no app in zone: " + zoneName);
-	        return appList.get(0);
-    	}
-    }
-
-    @Override
-    public List<EzyApp> getAppList() {
-    	List<EzyApp> list = new ArrayList<>();
-    	synchronized (this) {
-    		list.addAll(appList);
+	@Override
+	public void addApp(EzyApp app) {
+		synchronized (this) {
+			this.appList.add(app);
+			this.appsById.put(app.getId(), app);
+			this.appsByName.put(app.getName(), app);
 		}
-        return list;
-    }
+	}
 
-    @Override
-    public EzyApp getAppById(int appId) {
-    	synchronized (appList) {
-    		EzyApp app = appsById.get(appId);
-            return app;
+	@Override
+	public EzyApp removeApp(int appId) {
+		synchronized (this) {
+			EzyApp app = this.appsById.remove(appId);
+			if (app != null) {
+				this.appList.remove(app);
+				this.appsByName.remove(app.getName());
+			}
+			return app;
 		}
-    }
+	}
 
-    @Override
-    public EzyApp getAppByName(String appName) {
-        synchronized (this) {
-        	EzyApp app = appsByName.get(appName);
-            return app;
+	@Override
+	public EzyApp getApp() {
+		synchronized (this) {
+			if (appList.isEmpty())
+				throw new IllegalStateException("has no app in zone: " + zoneName);
+			return appList.get(0);
 		}
-    }
+	}
 
-    @Override
-    public void clear() {
-    	synchronized (this) {
-    		appList.clear();
-            appsById.clear();
-            appsByName.clear();
+	@Override
+	public List<EzyApp> getAppList() {
+		List<EzyApp> list = new ArrayList<>();
+		synchronized (this) {
+			list.addAll(appList);
 		}
-    }
+		return list;
+	}
+
+	@Override
+	public EzyApp getAppById(int appId) {
+		synchronized (appList) {
+			EzyApp app = appsById.get(appId);
+			return app;
+		}
+	}
+
+	@Override
+	public EzyApp getAppByName(String appName) {
+		synchronized (this) {
+			EzyApp app = appsByName.get(appName);
+			return app;
+		}
+	}
+
+	@Override
+	public void clear() {
+		synchronized (this) {
+			appList.clear();
+			appsById.clear();
+			appsByName.clear();
+		}
+	}
 }
